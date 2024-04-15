a3aa1cb8c1288fed324b6a35ed6c945d0077f3f4
Isabelle Vimercati <103614557+BelleVimercati@users.noreply.github.com>
2024-04-15 20:01:15 -0300

    feat: configuração de hook-form + zod do criar usuário

--- src/pages/AddUser/index.jsx
+++ src/pages/AddUser/index.jsx
@@ -5,7 +5,37 @@ import Input from "../../components/form/Input/Input";
 import ButtonPrimary from "../../components/buttonPrimary/ButtonPrimary";
 import styles from "../AddUser/index.module.css";
 
-function AddUser() {
+import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const AddUserFormSchema = z.object({
+  name: z.string().min(1, { message: "O nome é obrigatório" }),
+  email: z
+    .string()
+    .min(1, { message: "O email é obrigatório" })
+    .refine((email) => {
+      return email.endsWith("@consultoriafocus.com");
+    }, "Formato de email invalido"),
+  password: z.string().min(1, { message: "A senha é obrigatória" }),
+});
+
+const AddUser = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(AddUserFormSchema),
+  });
+
   return (
     <div className={styles.body}>
       <div className={styles.container}>
@@ -13,18 +43,41 @@ function AddUser() {
           <div className={styles.title}>
             <Title title="Adicionar" />
           </div>
-          <div className={styles.labels}>
-            <Input label="Nome" id="nome" type="text" />
-            <Input label="Email" id="email" type="email" />
-            <Input label="Senha" id="senha" type="password" />
-          </div>
-          <div className={styles.button}>
-            <ButtonPrimary text="Cadastrar" />
-          </div>
+
+          <form onSubmit={handleSubmit()}>
+            <div className={styles.labels}>
+              <Input label="Nome" id="nome" type="text" {...register("name")} />
+              {errors.name && (
+                <span className={styles.error}>{errors.name.message}</span>
+              )}
+              <Input
+                label="Email"
+                id="email"
+                type="email"
+                {...register("email")}
+              />
+              {errors.email && (
+                <span className={styles.error}>{errors.email.message}</span>
+              )}
+              <Input
+                label="Senha"
+                id="senha"
+                type="password"
+                {...register("password")}
+              />
+              {errors.password && (
+                <span className={styles.error}>{errors.password.message}</span>
+              )}
+            </div>
+            <div className={styles.button}>
+              <ButtonPrimary text="Cadastrar" />
+            </div>
+          </form>
         </Card>
       </div>
+      <DevTool control={control} />
     </div>
   );
-}
+};
 
 export default AddUser;
